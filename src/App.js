95ed1cc947054d9d9d2dc35cde765dcabdcb9e02
bearinawolfpack <bearinawolfpack@gmail.com>
2020-12-27 18:22:21 -0500

    Add NavbarComponent to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import GlobalStyle from './Themes/globalStyles';
+import NavbarComponent from './Components/navbarComponent';
 
 const App = () => {
   return (
     <>
+      <NavbarComponent />
       <GlobalStyle />
       <h1>Hello World</h1>
     </>
