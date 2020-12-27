a9c2cd10fae9a9235ddd4a250fc72152c9b9a838
bearinawolfpack <bearinawolfpack@gmail.com>
2020-12-27 17:57:50 -0500

    Add GlobalStyle to Themes dir

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalStyle from './Themes/globalStyles';
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <h1>Hello World</h1>
     </>
   );
