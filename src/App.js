02e6c0397736a3ab0c4b22af383ae7b9abcdc411
bearinawolfpack <bearinawolfpack@gmail.com>
2021-01-04 17:12:07 -0500

    Add React Router and Reacter Router routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import GlobalStyle from './Themes/globalStyles';
 import NavbarComponent from './Components/navbarComponent';
+import PortfolioComponent from './Components/PortfolioComponents/portfolioPage';
+import ProjectComponent from './Components/ProjectComponents/projectPage';
+import ArticleComponent from './Components/ArticleComponents/articlePage';
 
 const App = () => {
   return (
     <>
       <NavbarComponent />
       <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={PortfolioComponent} />¸
+          <Route path="/pricing" component={ProjectComponent} />¸
+          <Route path="/schedule" component={ArticleComponent} />¸
+        </Switch>
+      </Router>
       <h1>Hello World</h1>
     </>
   );
